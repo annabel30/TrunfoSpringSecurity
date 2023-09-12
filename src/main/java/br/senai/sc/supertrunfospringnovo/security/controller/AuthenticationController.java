c68160da07ca35c1588076bce74766ef76977999
Annabel <annabel_wulf@estudante.sesisenai.org.br>
2023-09-12 13:48:52 -0300

    att

--- src/main/java/br/senai/sc/supertrunfospringnovo/security/controller/AuthenticationController.java
+++ src/main/java/br/senai/sc/supertrunfospringnovo/security/controller/AuthenticationController.java
@@ -14,11 +14,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/authentication")
 public class AuthenticationController {
 
